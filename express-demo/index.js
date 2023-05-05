1fbb3a42aa7a3f177865d700a1bd9c32018b48be
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-05-04 19:15:22 -0600

    Se añaden nuevas llamadas del endpoint

--- express-demo/index.js
+++ express-demo/index.js
@@ -1,19 +1,33 @@
 //REST API demo in Node.js
-var express = require('express'); // requre the express framework
+var express = require("express"); // requre the express framework
 var app = express();
-var fs = require('fs'); //require file system object
+var fs = require("fs"); //require file system object
+var employeesData = require('./employees.json');;
 
 // Endpoint to Get a list of users
-app.get('/api/employees', function(req, res){
-    fs.readFile(__dirname + "/" + "employees.json", 'utf8', function(err, data){
-        console.log(data);
-        res.end(data); // you can also use res.send()
-    });
-})
+app.get("/api/employees", function (req, res) {
+  if(parseInt(req.query.page)==1){
+    res.end(JSON.stringify(employeesData.slice(0,2))); // you can also use res.send()
+  }
+  else if(parseInt(req.query.page)==2){
+    res.end(JSON.stringify(employeesData.slice(2,4))); // you can also use res.send()
+  }
+  else{
+    res.end(JSON.stringify(employeesData)); // you can also use res.send()
+  }
+
+});
+// Endpoint to Get a list of users
+app.get("/api/employees?page=1", function (req, res) {
+  console.log(employeesData.slice(1,2))
+  res.end(JSON.stringify(employeesData.slice(0,2))); // you can also use res.send()
+});
 
 // Create a server to listen at port 8080
-var server = app.listen(8080, function(){
-    var host = server.address().address
-    var port = server.address().port
-    console.log("REST API demo app listening at http://%s:%s", host, port)
-})
+var server = app.listen(8080, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("REST API demo app listening at http://%s:%s", host, port);
+});
+
+
