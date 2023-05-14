bd51720595b0774d48370c3ced0135b25bff9f75
MariangelaNM <91926755+MariangelaNM@users.noreply.github.com>
2023-05-13 18:10:28 -0600

    se crea la fase de autenticacion para el entregable

--- express-demo/controllers/controllers.mongoose.js
+++ express-demo/controllers/controllers.mongoose.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const Post = require("../models/model.mongoose");
+const User = require("../models/model.user.mongoose");
+const tools = require("../middlewares/auth.js");
+const bcrypt = require("bcrypt");
 
-const app = express();
-app.use(express.json());
+const router = express();
 
 // connect to in-memory database
 mongoose.connect("mongodb://localhost/mydatabase", {
@@ -12,7 +14,7 @@ mongoose.connect("mongodb://localhost/mydatabase", {
 });
 
 // create a new post
-app.post("/api/posts", async (req, res) => {
+router.post("/api/posts", tools.authenticate, async (req, res) => {
   try {
     const post = new Post({
       title: req.body.title,
@@ -28,7 +30,7 @@ app.post("/api/posts", async (req, res) => {
 });
 
 // get all posts
-app.get("/api/posts", async (req, res) => {
+router.get("/api/posts", tools.authenticate, async (req, res) => {
   try {
     const posts = await Post.find();
     res.status(200).json(posts);
@@ -38,7 +40,7 @@ app.get("/api/posts", async (req, res) => {
 });
 
 // get a single post by ID
-app.get("/api/posts/:id", async (req, res) => {
+router.get("/api/posts/:id", tools.authenticate, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post) {
@@ -52,7 +54,7 @@ app.get("/api/posts/:id", async (req, res) => {
 });
 
 // update a post by ID
-app.patch("/api/posts/:id", async (req, res) => {
+router.patch("/api/posts/:id", tools.authenticate, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post) {
@@ -76,7 +78,7 @@ app.patch("/api/posts/:id", async (req, res) => {
 });
 
 // delete a post by ID
-app.delete("/api/posts/:id", async (req, res) => {
+router.delete("/api/posts/:id", tools.authenticate, async (req, res) => {
   try {
     const post = await Post.findById(req.params.id);
     if (post) {
@@ -89,5 +91,50 @@ app.delete("/api/posts/:id", async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 });
+
+router.post("/api/users", async (req, res) => {
+  try {
+    const { name, email, password, bio } = req.body;
+    const saltRounds = 10;
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      bio,
+    });
+
+    await user.save();
+
+    res.status(201).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+router.post("/api/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(401).json({ message: "Credentials invalidad" });
+    }
+
+    const isValidPassword = await user.checkPassword(password);
+
+    if (!isValidPassword) {
+      return res.status(401).json({ message: "Credentials invalidad" });
+    }
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+
+    res.status(200).json({ token });
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 // start the server
-app.listen(3000, () => console.log("Server started on port 3000"));
+router.listen(3000, () => console.log("Server started on port 3000"));
---
+++ express-demo/middlewares/auth.js
@@ -0,0 +1,16 @@
+module.exports = {
+  // Middleware function to authenticate the JWT token in the request
+  authenticate: function authenticateToken(req, res, next) {
+    // Get the Authorization header with the JWT token
+    const authHeader = req.headers["authorization"];
+    const token = authHeader && authHeader.split(" ")[1];
+    // If the token is not found, return an HTTP 401 error
+    if (!token) return res.status(401).json({ message: "Invalid Token" });
+    // Check the token and get the payload
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+      if (err) return res.status(401).json({ message: "Invalid Token" });
+      req.user = payload;
+      next();
+    });
+  },
+};
