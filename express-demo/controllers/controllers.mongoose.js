f57f046eb2483ec3d33fbc3ea2406145c21567da
Mariangela NM <mariangelanavarro10@gmail.com>
2023-05-20 14:58:25 -0600

    Se ajusta tercera entrega

--- express-demo/controllers/controllers.mongoose.js
+++ express-demo/controllers/controllers.mongoose.js
@@ -2,15 +2,60 @@ const express = require("express");
 const mongoose = require("mongoose");
 const Post = require("../models/model.mongoose");
 const User = require("../models/model.user.mongoose");
-const tools = require("../middlewares/auth.js");
 const bcrypt = require("bcrypt");
 const jwt = require('jsonwebtoken');
+const tools = require("../middlewares/auth.js");
 const router = express();
 router.use(express.json())
 // connect to in-memory database
 mongoose.connect("mongodb://0.0.0.0:27017/People", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
+});
+
+
+// Ruta POST /api/login
+router.post('/api/login', async (req, res) => {
+  try {
+    const password = JSON.stringify({ 'password': req.body.password });
+    const email = JSON.stringify({ 'email': req.body.email });
+
+    // Validar los datos de entrada
+    if (!email || !password) {
+      return res.status(400).json({ error: 'Debes proporcionar email y contraseña' });
+    }
+    const posts = await User.find();
+    // Buscar al usuario por su email
+    const user = posts.find(user => user.email === req.body.email);
+
+    // Verificar si el usuario existe
+    if (!user) {
+      return res.status(401).json({ error: 'Credenciales inválidas' });
+    }
+
+    // Verificar la contraseña
+    await bcrypt.compare(req.body.password, user.password, (err, result) => {
+      if (err) {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+      }
+
+      if (result) {
+        console.log('La contraseña coincide');
+        return true;
+      } else {
+        return res.status(401).json({ error: 'Credenciales inválidas' });
+      }
+    });
+
+    // Generar el token JWT de sesión
+    const token = jwt.sign({ userId: user._id }, 'secreto', { expiresIn: '1h' });
+
+    // Devolver el token JWT en la respuesta
+    return res.json({ token });
+  } catch (error) {
+    console.error('Error al realizar el inicio de sesión:', error);
+    return res.status(500).json({ error: 'Error en el servidor' });
+  }
 });
 
 // create a new post
@@ -41,7 +86,7 @@ router.get("/api/posts", tools.authenticate, async (req, res) => {
 
 // get a single post by ID
 
-router.get("/api/posts/:_id", async (req, res) => {
+router.get("/api/posts/:_id", tools.authenticate, async (req, res) => {
 
   try {
     const post = await Post.findById(req.params._id);
@@ -56,7 +101,7 @@ router.get("/api/posts/:_id", async (req, res) => {
 });
 
 // update a post by ID
-router.patch("/api/posts/:_id", async (req, res) => {
+router.patch("/api/posts/:_id", tools.authenticate, async (req, res) => {
 
   try {
     const post = await Post.findById(req.params._id);
@@ -81,11 +126,10 @@ router.patch("/api/posts/:_id", async (req, res) => {
 });
 
 // delete a post by ID
-router.delete("/api/posts/:_id", async (req, res) => {
+router.delete("/api/posts/:_id", tools.authenticate, async (req, res) => {
 
   try {
 
-    console.log(await Post.findById(req.params._id));
     const post = await Post.findById(req.params._id);
     if (post) {
       await Post.deleteMany({ _id: req.params._id });
@@ -100,15 +144,15 @@ router.delete("/api/posts/:_id", async (req, res) => {
 
 router.post("/api/users", async (req, res) => {
   try {
-    const { name, email, password, bio } = req.body;
+
     const saltRounds = 10;
-    const hashedPassword = await bcrypt.hash(password, saltRounds);
+    const hashedPassword = await bcrypt.hash(req.body.password, saltRounds);
 
     const user = new User({
-      name,
-      email,
+      name: req.body.name,
+      email: req.body.email,
       password: hashedPassword,
-      bio,
+      bio: req.body.bio,
     });
 
     await user.save();
@@ -118,28 +162,13 @@ router.post("/api/users", async (req, res) => {
     res.status(400).json({ message: error.message });
   }
 });
-router.post("/api/login", async (req, res) => {
-
-  const email=req.body.email;
-  
-  const password=req.body.password;
+// get all users
+router.get("/api/user", async (req, res) => {
   try {
-    const user = JSON.stringify({'user':email});
-    if (!user) {
-      return res.status(401).json({ message: "Credentials invalidad" });
-    }
-
-    const isValidPassword = JSON.stringify({'password':password});
-
-    if (!isValidPassword) {
-      return res.status(401).json({ message: "Credentials invalidad" });
-    }
-
-    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
-
-    res.status(200).json({ token });
-  } catch (err) {
-    res.status(400).json({ message: err.message });
+    const posts = await User.find();
+    res.status(200).json(posts);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
--- express-demo/middlewares/auth.js
+++ express-demo/middlewares/auth.js
@@ -1,16 +1,20 @@
+const bcrypt = require("bcrypt");
+const jwt = require('jsonwebtoken');
 module.exports = {
   // Middleware function to authenticate the JWT token in the request
   authenticate: function authenticateToken(req, res, next) {
     // Get the Authorization header with the JWT token
-    const authHeader = req.headers["authorization"];
-    const token = authHeader && authHeader.split(" ")[1];
-    // If the token is not found, return an HTTP 401 error
-    console.log("-",token)
-    if (!token) return res.status(401).json({ message: "Invalid Token" });
-    // Check the token and get the payload
-    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
-      if (err) return res.status(401).json({ message: "Invalid Token" });
-      req.user = payload;
+    const token = req.headers.authorization;
+    if (!token) {
+      return res.status(401).json({ error: 'Acceso no autorizado' });
+    }
+  
+    jwt.verify(token, 'secreto', (err, decodedToken) => {
+      if (err) {
+        return res.status(401).json({ error: 'Token inválido' });
+      }
+  
+      req.userId = decodedToken.userId;
       next();
     });
   },
